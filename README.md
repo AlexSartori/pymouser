548abec6f8483324765a0969c4fc67d5f08b8474
Alex Sartori <alex.sartori1997@gmail.com>
2023-03-21 20:55:21 +0100

    Implement search by part-number

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PyMouser
+
+## Installation
+
+Install the package with pip.
+
+```pip install --user pymouser```
+
+## Usage:
+
+```python
+import pymouser
+
+
+# Initialize the package with your API key
+mouser = pymouser.MouserAPI('your-search-key')
+
+# Search by Part-Number
+err, res = mouser.search_by_PN('your-part-number')
+
+# Check for errors or print the returned results
+if err:
+    print("Error during request:")
+    print(err)
+else:
+    if res['NumberOfResult'] == 0:
+        print("No results matched the part number")
+    else:
+        for match in res['Parts']:
+            print("Match for PartNumber .... %s" % match['MouserPartNumber'])
+            print("Description ............. %s" % match['Description'])
+            print("Link to datasheet ....... %s" % match['DataSheetUrl'])
+            print("Link to product page .... %s" % match['ProductDetailUrl'])
+            print("")
+```
--- pymouser/mouser.py
+++ pymouser/mouser.py
@@ -1,3 +1,33 @@
-import requests
+import requests, json
 
-pass
+
+# Type aliases
+MouserItem = dict[str: str]
+
+
+class MouserAPI:
+    SEARCH_API_KEY: str = None
+
+    def __init__(self, search_api_key: str):
+        self.SEARCH_API_KEY = search_api_key
+
+    def search_by_PN(self, part_number: str) -> MouserItem:
+        req_url = "https://api.mouser.com/api/v1/search/partnumber?apiKey=%s" % self.SEARCH_API_KEY
+
+        req_body = {
+            "SearchByPartRequest": {
+                "mouserPartNumber": part_number,
+                "partSearchOptions": "Exact"
+            }
+        }
+
+        req_headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(url=req_url, data=json.dumps(req_body), headers=req_headers).json()
+        results = response['SearchResults']
+        errors = response['Errors']
+        
+        return (errors, results)
